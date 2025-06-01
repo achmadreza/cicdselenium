b930e3f98bbf6b54cbcdbdf4f30c333832518bf5
unknown <areszha@gmail.com>
2025-06-01 12:46:28 +0700

    add cucum

--- src/test/java/runner/TestRunner.java
+++ src/test/java/runner/TestRunner.java
@@ -8,7 +8,7 @@ import io.cucumber.junit.CucumberOptions;
 @CucumberOptions(
   features = "src/test/resources/features",
   glue = "stepdefinitions",
-  plugin = {"pretty", "json:target/cucumber.json"},
+  plugin = {"pretty", "json:target/cucumber-reports/Cucumber.json"},
   monochrome = true
 )
 public class TestRunner {
